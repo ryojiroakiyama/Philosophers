3ee9364c63949bce850c0c0515affc3baa6cd41b
rakiyama <ryojiro25@gmail.com>
2021-10-07 14:48:49 +0900

    add pthread function

--- srcs/main.c
+++ srcs/main.c
@@ -77,92 +77,124 @@ int	ft_atoi(char *str, bool *nonnum_check)
 	return ((int)num * sign);
 }
 
-// void	*philosopher(void *var)
-// {
-// 	pthread_mutex_lock(&mutex);
-// 	printf("in thread_func:%d\n", *(int *)var);
-// 	pthread_mutex_unlock(&mutex);
-// 	return (0);
-// }
-
-char	set_init_data(char **argv, t_init_data *init_data)
+char	set_state_time(char **argv, t_state *state)
 {
 	bool	nonnum_check;
 
-	init_data->number_of_philosophers = ft_atoi(argv[1], &nonnum_check);
-	if (init_data->number_of_philosophers < 0 || nonnum_check)
+	state->number_of_philosophers = ft_atoi(argv[1], &nonnum_check);
+	if (state->number_of_philosophers < 0 || nonnum_check)
 		return (1);
-	init_data->time_to_die = ft_atol(argv[2], &nonnum_check);
-	if (init_data->time_to_die < 0 || nonnum_check)
+	state->time_to_die = ft_atol(argv[2], &nonnum_check);
+	if (state->time_to_die < 0 || nonnum_check)
 		return (1);
-	init_data->time_to_eat = ft_atol(argv[3], &nonnum_check);
-	if (init_data->time_to_eat < 0 || nonnum_check)
+	state->time_to_eat = ft_atol(argv[3], &nonnum_check);
+	if (state->time_to_eat < 0 || nonnum_check)
 		return (1);
-	init_data->time_to_sleep = ft_atol(argv[4], &nonnum_check);
-	if (init_data->time_to_sleep < 0 || nonnum_check)
+	state->time_to_sleep = ft_atol(argv[4], &nonnum_check);
+	if (state->time_to_sleep < 0 || nonnum_check)
 		return (1);
 	if (argv[5])
 	{
-		init_data->time_tobe_satisfied = ft_atol(argv[5], &nonnum_check);
-		if (init_data->time_tobe_satisfied < 0 || nonnum_check)
+		state->time_tobe_satisfied = ft_atol(argv[5], &nonnum_check);
+		if (state->time_tobe_satisfied < 0 || nonnum_check)
 			return (1);
 	}
 	else
-		init_data->time_tobe_satisfied = -1;
+		state->time_tobe_satisfied = -1;
 	return (0);
 }
 
-char	terminate_by_invalid_arg(void)
+char	put_invalid_arg_error(void)
 {
 	printf("invalid argument format\n");
 	printf("type and order of arguments required:\n");
-	printf("1:number_of_philosophers\n");
-	printf("2:time_to_die\n");
-	printf("3:time_to_eat\n");
-	printf("4:time_to_sleep\n");
-	printf("5:number_of_times_each_philosopher_must_eat (is optional)\n");
+	printf("1: number_of_philosophers\n");
+	printf("2: time_to_die\n");
+	printf("3: time_to_eat\n");
+	printf("4: time_to_sleep\n");
+	printf("5: number_of_times_each_philosopher_must_eat (is optional)\n");
 	return (1);
 }
 
-void	put_init_data(t_init_data *init_data)
+void	put_state(t_state *state)
 {
-	printf("init_data:\n");
-	printf("time_from_start        %ld\n", init_data->time_from_start);
-	printf("number_of_philosophers %d\n", init_data->number_of_philosophers);
-	printf("time_to_die            %ld\n", init_data->time_to_die);
-	printf("time_to_eat            %ld\n", init_data->time_to_eat);
-	printf("time_to_sleep          %ld\n", init_data->time_to_sleep);
-	printf("time_tobe_satisfied    %ld\n", init_data->time_tobe_satisfied);
+	printf("state:\n");
+	printf("time program start     %ld\n", state->time_last_eat);
+	printf("number_of_philosophers %d\n", state->number_of_philosophers);
+	printf("time_to_die            %ld\n", state->time_to_die);
+	printf("time_to_eat            %ld\n", state->time_to_eat);
+	printf("time_to_sleep          %ld\n", state->time_to_sleep);
+	printf("time_tobe_satisfied    %ld\n", state->time_tobe_satisfied);
 }
 
-char	terminate_by_function_error(char *message)
+char	put_error(char *message)
 {
 	printf("%s\n", message);
 	return (1);
 }
 
-int	main(int argc, char **argv)
+char	set_state(int argc, char **argv, t_state *state)
 {
 	struct timeval 	tv;
-	t_init_data		init_data;
 
 	if (gettimeofday(&tv, NULL) == -1)
-		return (terminate_by_function_error("gettimeofday"));
-	init_data.time_from_start = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-	if (!(5 <= argc && argc <= 6) || set_init_data(argv, &init_data))
-		return (terminate_by_invalid_arg());
-	put_init_data(&init_data);
-	// pthread_mutex_init(&mutex, NULL);
-	// var = 0;
-	// pthread_create(&thre, NULL, &thread_func, &var);
-	// pthread_mutex_lock(&mutex);
-	// cnt = 50000;
-	// while (cnt--)
-	// 	var++;
-	// pthread_mutex_unlock(&mutex);
-	// printf("in main:%d\n", var);
-	// pthread_join(thre, NULL);
-	// pthread_mutex_destroy(&mutex);
-	// system("leaks a.out");
+		return (put_error("gettimeofday"));
+	state->time_last_eat = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	if (!(5 <= argc && argc <= 6) || set_state_time(argv, state))
+		return (put_invalid_arg_error());
+	state->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * state->number_of_philosophers);
+	if (!state->forks)
+		return (put_error("malloc"));
+	state->threads = (pthread_t *)malloc(sizeof(pthread_t) * state->number_of_philosophers);
+	if (!state->threads)
+	{
+		free(state->forks);
+		return (put_error("malloc"));
+	}
+	return (0);
+}
+
+void	*philosopher(void *state)
+{
+	state = (t_state *)state;
+//	pthread_mutex_lock(&mutex);
+	put_state(state);
+//	pthread_mutex_unlock(&mutex);
+	return (0);
+}
+
+char	run_thread(t_state *state)//どうやってそれぞれのスレッド（フィロソファ)に何番目なのか知らせる、構造体の変数使ったら他も共有してるから、、
+{
+	int	thread_cnt;
+
+	thread_cnt = 0;
+	while (thread_cnt < state->number_of_philosophers)
+	{
+		pthread_mutex_init(state->forks + thread_cnt, NULL);
+		pthread_create(state->threads + thread_cnt, NULL, &philosopher, state);
+		thread_cnt++;
+	}
+	thread_cnt = 0;
+	while (thread_cnt < state->number_of_philosophers)
+	{
+		pthread_join(state->threads[thread_cnt], NULL);
+		pthread_mutex_destroy(state->forks + thread_cnt);
+		thread_cnt++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_state	state;
+
+	if (set_state(argc, argv, &state))
+		return (1);
+	if (run_thread(&state))
+		return (1);
+	free(state.forks);
+	free(state.threads);
+	// if (system("leaks a.out >/dev/null"))
+	// 	system("leaks a.out");
 	return (0);
 }
--- srcs/philo.h
+++ srcs/philo.h
@@ -10,24 +10,15 @@
 
 typedef struct s_state
 {
+	pthread_t		*threads;
 	pthread_mutex_t	*forks;
-	int				order;
-	long			time_to_die;
-	long			time_from_last_eat;
-	long			time_to_eat;
-	long			time_to_sleep;
-	long			time_tobe_satisfied;
-	char			someone_died;
-}	t_state;
-
-typedef struct s_init_data
-{
-	long			time_from_start;
 	int				number_of_philosophers;
 	long			time_to_die;
 	long			time_to_eat;
 	long			time_to_sleep;
+	long			time_last_eat;
 	long			time_tobe_satisfied;
-}	t_init_data;
+	char			someone_died;
+}	t_state;
 
 #endif
