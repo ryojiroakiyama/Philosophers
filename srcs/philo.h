e01f106cd2b416e4a955e8580d7139426e503a9a
rakiyama <ryojiro25@gmail.com>
2021-11-04 19:48:50 +0900

    make philo and monitor

--- srcs/philo.h
+++ srcs/philo.h
@@ -8,17 +8,21 @@
 #include <libc.h>
 #include <stdbool.h>
 
+// status
 # define SUCCESS 0
 # define FAIL 1
+
+// flag some one died or not
 # define NO_ONE_DIED 0
 # define SOME_ONE_DIED 1
 
+// message to put there status
 # define EAT "eating"
 # define SLEEP "sleeping"
 # define THINK "thinking"
 # define DIE "died"
 
-//color settings
+// color settings
 # define RED "\033[31m"
 # define BLUE "\033[34m"
 # define CYAN "\033[36m"
@@ -32,49 +36,75 @@
 typedef enum e_access
 {
 	READ,
-	EDIT
+	EDIT,
+	ACCESS_NUM
 }	t_access;
 
-typedef struct s_monitor_data
+typedef enum e_memory
 {
-	long			time_to_die;
-	long			time_tobe_satisfied;
-	long			*time_last_eat;
-	pthread_t		thread_id;
-	pthread_mutex_t	*last_eat_mutex;
-	pthread_mutex_t	*death_mutex;
-	char			*death_flag;
-}	t_monitor_data;
+	INIT,
+	FREE,
+	MEMORY_NUM
+}	t_memory;
 
-typedef struct s_philo_data
+typedef enum e_mutex
 {
-	int				order;
-	long			time_to_die;
-	long			time_to_eat;
-	long			time_to_sleep;
-	long			time_tobe_satisfied;
-	long			time_last_eat;
-	pthread_t		thread_id;
-	pthread_mutex_t	*right_fork;
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*put_mutex;
-	pthread_mutex_t	last_eat_mutex;
-	pthread_mutex_t	*death_mutex;
-	char			*death_flag;
-}	t_philo_data;
+	RIGHT_FORK,
+	LEFT_FORK,
+	PUT,
+	MEAL,
+	DEATH,
+	MUTEX_NUM
+}	t_mutex;
 
+typedef enum e_time
+{
+	TO_DIE,
+	TO_EAT,
+	TO_SLEEP,
+	BE_STUFFED,
+	LAST_EAT,
+	TIME_NUM
+}	t_time;
+
+typedef struct s_thread_data
+{
+	pthread_t				thread_id;
+	int						order;
+	long					time[TIME_NUM];
+	long					*time_last_eat;// = &time[LAST_EAT]
+	char					*death_flag;// = &mdata->death_flag
+	pthread_mutex_t			*mutex[MUTEX_NUM];
+	struct s_thread_data	*monitor;// monitor->time_last_eat = &time[LAST_EAT]
+	//								   monitor->death_flag = &mdata->death_flag
+}	t_thread_data;
+
+/*
+** Members as pointer type
+** (Memory will be allocated for
+** sizeof(type) x (number of philosophers)).
+**   philos : philos[0] has data of first philosopher's thread.
+**   times_last_eat : times_last_eat[0] is shared by philos[0] <-> monitors[0].
+**   forks : forks[0] is shared by philos[0] <-> philos[1].
+**   last_eat : last_eat[0] is used to lock access to times_last_eat[0].
+**
+** Else members
+**   time : Values of arguments.
+**   death_flag : shared by all threads.
+**   put : put is used to lock use of put_status().
+**   death : death is used to lock access to death_flag.
+*/
 typedef struct s_manage_data
 {
 	int				number_of_philosophers;
-	long			time_to_die;
-	long			time_to_eat;
-	long			time_to_sleep;
-	long			time_tobe_satisfied;
-	t_philo_data	*philos;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	put_mutex;
-	pthread_mutex_t	death_mutex;
+	long			time[TIME_NUM];
 	char			death_flag;
+	t_thread_data	*philos;
+	t_thread_data	*monitors;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*last_eat;
+	pthread_mutex_t	put;
+	pthread_mutex_t	death;
 }	t_manage_data;
 
 #endif
