ec44a2086891a4643347fbc44f5f14cd4b1032ce
rakiyama <ryojiro25@gmail.com>
2021-12-17 13:03:11 +0900

    add lab

--- lab/mutex.c
+++ lab/mutex.c
@@ -16,16 +16,22 @@ int	main(void)
 	pthread_t	thread_id;
 
 	var = 0;
-	//pthread_mutex_init(&mutex, NULL);
-	//pthread_create(&thread_id, NULL, &thread_func, &var);
-	//pthread_mutex_lock(&mutex);
-	//while (var < 50000)
-	//	var++;
-	//pthread_mutex_unlock(&mutex);
-	//printf("in main:%d\n", var);
-	//pthread_join(thread_id, NULL);
-	mutex.__sig = 10000;
-	mutex.__opaque[1] = 1;
+	pthread_mutex_init(&mutex, NULL);
+	pthread_create(&thread_id, NULL, &thread_func, &var);
+	pthread_mutex_lock(&mutex);
+	while (var < 50000)
+		var++;
+	pthread_mutex_unlock(&mutex);
+	printf("in main:%d\n", var);
+	pthread_join(thread_id, NULL);
+	//mutex.__sig = 10000;
+	//mutex.__opaque[1] = 1;
+	pthread_mutex_destroy(&mutex);
+	pthread_mutex_destroy(&mutex);
+	pthread_mutex_destroy(&mutex);
+	pthread_mutex_destroy(&mutex);
+	pthread_mutex_destroy(&mutex);
+	pthread_mutex_destroy(&mutex);
 	pthread_mutex_destroy(&mutex);
 	//pthread_mutex_destroy(NULL);
 	return (0);
